d1eb59c66f49abb1ba68d4268bfb709af9d770be
Olivier Meyer <looolive@gmail.com>
2022-10-20 17:13:49 +0200

    Add categories API calls

--- js/getfilms.js
+++ js/getfilms.js
@@ -21,32 +21,48 @@ function prom(url) {
     return p
 }
 
-async function getFilms(url, query_selector) {
-    var content = await prom(url) // resolved value of the promise
+async function getFilms(cat) {
+    switch (cat) {
+        case 'tops':
+            var url = DOMAIN + 'api/v1/titles/?sort_by=-imdb_score&page_size=7'
+            var section = 'section_top'
+            break;
+        case 'cat_1':
+            var url = DOMAIN + 'api/v1/titles/?sort_by=-imdb_score&page_size=7&genre=music'
+            var section = 'section_1'
+            break;
+        case 'cat_2':
+            var url = DOMAIN + 'api/v1/titles/?sort_by=-imdb_score&page_size=7&genre=Drama'
+            var section = 'section_2'
+            break;
+        case 'cat_3':
+            var url = DOMAIN + 'api/v1/titles/?sort_by=-imdb_score&page_size=7&genre=Comedy'
+            var section = 'section_3'
+            break;
+    }
+    var content = await prom(url)
     contentJSON = JSON.parse(content)
     results = contentJSON.results
-    document.querySelector(query_selector).innerHTML = ``
+    document.getElementById(section).innerHTML = ''
+    for (var film in results) {
+        document.getElementById(section).innerHTML += `
+        <div class="item" onclick="openModal('${results[film].id}')">
+            <img src="${results[film].image_url}" alt="">
+        </div>`
+    }
 }
 
-
-async function getBestFilmDetails(callback_modal) {
+async function getFilmDetails(film_id) {
     /** 
-     * Get unique best film details from api endpoint
-     * Then write html in main block and in the modal window
+     * Get unique film details from api endpoint
+     * Then write html infos in the modal window
      * NB : Async functions always return a promise
-     * 
-     * Pass openModal as a callback to this function, so that it will be called AFTER button "open-modal" is created
      */
 
-    // Get best film endpoint
-    var content = await prom(DOMAIN + 'api/v1/titles/?sort_by=-imdb_score&page_size=1') // resolved value of the promise
-    contentJSON = JSON.parse(content)
-    results = contentJSON.results
-    id = results[0].id
-    url_best_film = DOMAIN + 'api/v1/titles/' + id
-
-    // get best film details
-    var details = await prom(url_best_film)
+    // Get film endpoint
+    var url = DOMAIN + 'api/v1/titles/' + film_id
+    // get film details
+    var details = await prom(url) // resolved value of the promise
     detailsJSON = JSON.parse(details)
     console.log(detailsJSON)
     title = detailsJSON.title
@@ -59,19 +75,8 @@ async function getBestFilmDetails(callback_modal) {
     actors = detailsJSON.directors.join(", ")
     duration = detailsJSON.duration
     countries = detailsJSON.countries.join(", ")
-    box_office = detailsJSON.worldwide_gross_income
+    box_office = detailsJSON.worldwide_gross_income ? detailsJSON.worldwide_gross_income : "NC"
     resume = detailsJSON.long_description
-    
-    // Write details to block best film
-    infos = document.querySelector('#infos_best')
-    infos.innerHTML = `
-        <h1>${title}</h1>
-        <p>${resume}</p>
-        <div class="Stars" style="--rating: ${score / 2};" title="Imdb score : ${score}"></div>
-        <div><a href="#" class="button open-modal">En savoir plus</a></div>`  
-    main_poster = document.querySelector('#main_poster')
-    main_poster.innerHTML = `
-        <img src="${img_url}" />`
 
     // Write details to modal
     modal_title = document.querySelector('.modal h3')
@@ -96,49 +101,56 @@ async function getBestFilmDetails(callback_modal) {
     modal_poster = document.querySelector('#modal_poster')
     modal_poster.innerHTML = `
         <img src="${img_url}" />`
-
-    callback_modal();
 }
 
 
-async function getBestFilm(url) {
-    /* Async functions always return a promise */
-    var content = await prom(url) // resolved value of the promise
+async function getBestFilmDetails() {
+    /** 
+     * Get unique best film details from api endpoint
+     * Then write html in main block and in the modal window
+     * NB : Async functions always return a promise
+     */
+
+    // Get best film endpoint
+    var content = await prom(DOMAIN + 'api/v1/titles/?sort_by=-imdb_score&page_size=1') // resolved value of the promise
     contentJSON = JSON.parse(content)
     results = contentJSON.results
-    // txt infos
+    id = results[0].id
+    url_best_film = DOMAIN + 'api/v1/titles/' + id
+
+    // get best film details
+    var details = await prom(url_best_film)
+    detailsJSON = JSON.parse(details)
+    title = detailsJSON.title
+    img_url = detailsJSON.image_url
+    score = parseFloat(detailsJSON.imdb_score)
+    resume = detailsJSON.long_description
+
+    // Write details to block best film
     infos = document.querySelector('#infos_best')
-    title = results[0].title
-    director = results[0].directors[0]
-    actors = results[0].actors.join(", ")
-    score = parseFloat(results[0].imdb_score) / 2
     infos.innerHTML = `
         <h1>${title}</h1>
-        <p><strong>Réalisateur</strong> : ${director}</p>
-        <p><strong>Avec</strong> : ${actors}</p>
-        <div class="Stars" style="--rating: ${score};" title="Imdb score"></div>
-        <div><a href="#" class="button open-modal">En savoir plus</a></div>`
-
-    // img
-    poster = document.querySelector('#poster')
-    img_url = results[0].image_url
-    poster.innerHTML = `
+        <p>${resume}</p>
+        <div class="Stars" style="--rating: ${score / 2};" title="Imdb score : ${score}"></div>
+        <div><a href="#" class="button" onclick="openModal('${id}')">En savoir plus</a></div>`
+    main_poster = document.querySelector('#main_poster')
+    main_poster.innerHTML = `
         <img src="${img_url}" />`
-
 }
 
 
-function openModal() {
-    
-    const modal = document.querySelector(".modal");
-    const overlay = document.querySelector(".overlay");
-    const openModalBtn = document.querySelector(".open-modal"); // todo modifier avec querySelectorAll (sinon ne prend que le premier) + vérifier que l'élément est bien chargé
-    const closeModalBtn = document.querySelector(".btn-close");
+function openModal(film_id) {
+
+    getFilmDetails(film_id)
+
+    const modal = document.querySelector(".modal")
+    const overlay = document.querySelector(".overlay")
+    const closeModalBtn = document.querySelector(".btn-close")
 
     // close modal function
     const closeModal = function () {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
+        modal.classList.add("hidden")
+        overlay.classList.add("hidden")
     };
 
     // close the modal when the close button and overlay is clicked
@@ -147,17 +159,11 @@ function openModal() {
 
     // close modal when the Esc key is pressed
     document.addEventListener("keydown", function (e) {
-    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
-        closeModal();
-    }
+        if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+            closeModal()
+        }
     });
 
-    // open modal function
-    const openModal = function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-    };
-    // open modal event
-    openModalBtn.addEventListener("click", openModal);
-
+    modal.classList.remove("hidden")
+    overlay.classList.remove("hidden")
 }
